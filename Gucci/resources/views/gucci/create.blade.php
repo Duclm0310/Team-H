5289982c0f9762602a79e5279fbf143e2703efbf
nghiadepzaivietnam <NghiaNTGCH210333@fpt.edu.vn>
2023-06-21 11:49:02 +0700

    fe  view gucci

--- Gucci/resources/views/gucci/create.blade.php
+++ Gucci/resources/views/gucci/create.blade.php
@@ -1,70 +1,84 @@
 @extends('layout.layout')
-@section('tile','create new product')
+@section('title', 'Create New Product')
 @section('content')
 
-<section classs="form-box">
-  {{-- enctype="multipart/form-data" để có thế upload file --}}
-<form method="post" action="/gucci" enctype="multipart/form-data">
-    @csrf
-    <div class="mb-3">
-        <label for="title" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Product name">
-    </div>
-    <div class="mb-3">
-        <label for="author" class="form-label">Price</label>
-        <input type="text" class="form-control" id="price" name="price" placeholder="Price">
-    </div>
-    <div class="mb-3">
-        <label for="author" class="form-label">Material</label>
-        <select id="material" name="material" class="form-control">
-            <option selected="selected">Choose material</option>
-            @foreach($material as $material)
-            <option value="{{$material->id}}">{{$material->name}}</option>
-            @endforeach
-        </select>
-    </div>
-    <div class="mb-3">
-      <label for="author" class="form-label">Designer</label>
-      <select id="designer" name="designer" class="form-control">
-          <option selected="selected">Choose designer</option>
-          @foreach($designer as $designer)
-          <option value="{{$designer->id}}">{{$designer->name}}</option>
-          @endforeach
-      </select>
-  </div>
-  <div class="form-group">
-    <label for="sales">Sales</label>
-    <select name="sales[]" id="sales" class="form-control" multiple>
-        @foreach($sale as $sale)
-        <option value="{{$sale->id}}">{{$sale->name}}</option>
-        @endforeach
-    </select>
-</div>
-  <div class="mb-3">
-    <label for="author" class="form-label">Count</label>
-    <input type="text" class="form-control" id="count" name="count" placeholder="Count">
-  </div>
-    <div class="mb-3">
-      <label for="biography" class="form-label">Biography</label>
-      <textarea class="form-control" id="biography" name="biography" rows="3"></textarea>
-  </div>
-  <div class="form-group">
-    <label for="photo">Attach a photograph</label>
-    <input type="file" name="photo" id="photo" accept="image/*" class="form-control-file">
-  </div>
-  <div class="form-group">
-    <button type="submit" class="btn btn-outline-primary">Submit</button>
-  </div>
-  </form>
-</section>
-
 <style>
-   .form-box{
+    .background-image {
+        background-image: url('{{ asset('img/bg.jpg') }}');
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
         display: flex;
         justify-content: center;
-        flex-direction: column;
         align-items: center;
-        margin-bottom: 10px
-      }
-  </style>
-  @endsection
+        padding: 0;
+        margin: 0;
+    }
+
+    .form-box {
+        background-color: rgba(255, 255, 255, 0.8);
+        max-width: 400px;
+        padding: 20px;
+        border-radius: 10px;
+    }
+</style>
+
+<div class="background-image">
+    <section class="form-box">
+        {{-- enctype="multipart/form-data" để có thế upload file --}}
+        <form method="post" action="/gucci" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Product name">
+            </div>
+            <div class="mb-3">
+                <label for="author" class="form-label">Price</label>
+                <input type="text" class="form-control" id="price" name="price" placeholder="Price">
+            </div>
+            <div class="mb-3">
+                <label for="author" class="form-label">Material</label>
+                <select id="material" name="material" class="form-control">
+                    <option selected="selected">Choose material</option>
+                    @foreach($material as $material)
+                    <option value="{{$material->id}}">{{$material->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="author" class="form-label">Designer</label>
+                <select id="designer" name="designer" class="form-control">
+                    <option selected="selected">Choose designer</option>
+                    @foreach($designer as $designer)
+                    <option value="{{$designer->id}}">{{$designer->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="sales">Sales</label>
+                <select name="sales[]" id="sales" class="form-control" multiple>
+                    @foreach($sale as $sale)
+                    <option value="{{$sale->id}}">{{$sale->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="author" class="form-label">Count</label>
+                <input type="text" class="form-control" id="count" name="count" placeholder="Count">
+            </div>
+            <div class="mb-3">
+                <label for="biography" class="form-label">Biography</label>
+                <textarea class="form-control" id="biography" name="biography" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="photo">Attach a photograph</label>
+                <input type="file" name="photo" id="photo" accept="image/*" class="form-control-file">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-outline-primary">Submit</button>
+            </div>
+        </form>
+    </section>
+</div>
+
+@endsection
--- Gucci/resources/views/gucci/index.blade.php
+++ Gucci/resources/views/gucci/index.blade.php
@@ -1,127 +1,106 @@
 @extends('layout.layout')
-@section('tile','gucci')
+@section('title', 'gucci')
 @section('content')
-
-  <div class="row">
+  <div class="row" style="margin-top: 30px" >
     @foreach ($gucci as $gucci)
-    <div class="ad col-lg-4">
-      <div class="item">
-        
-              <a>
-                  <img src="{{url("/upload/".$gucci->photo)}}" class="card-img-top" alt="">
+      <div class="ad col-lg-4" style="height: 35%">
+        <div class="item">
+          <a>
+            <img src="{{ url("/upload/".$gucci->photo) }}" class="item-image" alt="">
+          </a>
+          <div class="brand">{{ $gucci->name }}</div>
+          <div class="sex">{{ $gucci->material->name }}</div>
+          <div class="sex">{{ $gucci->designer->name }}</div>
+          <div class="sex">
+            @foreach($gucci->sales as $sale)
+              <span class="sale-name">{{ $sale->name }}</span>
+            @endforeach
+          </div>
+          <div class="price">{{ $gucci->price }}</div>
+          <div class="action">
+            <div class="btn-group">
+              <a href="{{ url("/gucci/".$gucci->id) }}" class="btn btn-round btn-show" title="Show">
+                <i class="fas fa-eye"></i>
               </a>
-              <div class="brand">{{$gucci->name}}</div>
-              <div class="sex">{{$gucci->material->name}}</div>
-              <div class="sex">{{$gucci->designer->name}}</div>
-              <div class="sex">
-                  @foreach($gucci->sales as $sale)
-                       <span >{{$sale->name}}</span>
-                  @endforeach
-              </div>
-              <div class="price">{{$gucci->price}}</div>
-              <div class="action">
-                <button><a href="{{url("/gucci/".$gucci->id)}}">Show</a></button>
-                <button><a href="{{url("/gucci/".$gucci->id."/edit")}}">Edit</a></button>
-                <form method="post" action="{{url("/gucci/".$gucci->id)}}" class="d-inline">
-                {{method_field('DELETE')}}
+              <a href="{{ url("/gucci/".$gucci->id."/edit") }}" class="btn btn-round btn-edit" title="Edit">
+                <i class="fas fa-edit"></i>
+              </a>
+              <form method="post" action="{{ url("/gucci/".$gucci->id) }}" class="d-inline">
+                {{ method_field('DELETE') }}
                 @csrf
-                <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Are u sure?')">Delete</button>
-                </form>
-              </div>
-      </div>
+                <button class="btn btn-round btn-delete" type="submit" onclick="return confirm('Are you sure?')"
+                  title="Delete">
+                  <i class="fas fa-trash-alt"></i>
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-      @endforeach
+      </div>
+    @endforeach
   </div>
-   {{-- <table class="table table-striped
-   table-hover	
-   table-borderless
-   table-primary
-   align-middle">
-       <thead class="table-light">
-           <h1 class="h1">Product</h1>
-           <tr>
-               <th>Id</th>
-               <th>Name</th>
-               <th>Price</th>
-               <th>Material</th>
-               <th>Biography</th>
-               <th>Action</th>
-           </tr>
-           </thead>
-           <tbody class="table-group-divider">
-             @foreach ($gucci as $gucci)
-               <tr class="table-primary" >
-                 <td>{{$gucci->id}}</td>
-                 <td>
-                   <a>
-                     {{$gucci->name}}
-                   </a>
-                 </td>
-                   <td>{{$gucci->price}}</td>
-                   <td>
-                     <a href="{{url("/material/".$gucci->material->id)}}">
-                     {{$gucci->material->name}}
-                     </a>
-                   </td>
-                   <td>{{$gucci->biography}}</td>
-                   <td>
-                     <button><a href="{{url("/gucci/".$gucci->id)}}">Show</a></button>
-                     <button><a href="{{url("/gucci/".$gucci->id."/edit")}}">Edit</a></button>
-                     <form method="post" action="{{url("/gucci/".$gucci->id)}}" class="d-inline">
-                     {{method_field('DELETE')}}
-                     @csrf
-                     <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Are u sure?')">Delete</button>
-                     </form>
-                   </td>
-               </tr>
-               @endforeach
-           </tbody>
-        
-   </table> --}}
-
-
 @endsection
 
 <style>
-  .ad{
-		margin: 10px 0;
-		width: 30%;
-    margin: 30px 0;
+  /* Existing CSS styles */
 
-	}
-	.item{
-		border: 3px solid #f4f1f1;
-		text-align:left;
-		height: 100%;
-	}
-	.item img{
-		width: 100%;
-		height: 40%;
-	}
-	.item img:hover{
-		transform: scale(1.1,1.1);
-	}
-
-	.sex,.brand,.price{
-		padding:0 0 5px 10px;
-	}
-	.sex{
-		font-size: 0.7rem;
-		color: rgb(103, 95, 95);
-	}
-	.brand{
-		font-weight: 500;
-	}
-	.price{
-		font-size: 1.2rem;
-		font-weight: bold;
-		margin-bottom: 10px;
-	}
-	.ad{
-		margin: 50px 0;
-	}
-  .action{
-    padding-left: 60px;
-    margin: 20px 0;
+  .btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    background-color: #f4f1f1;
+    border: none;
+    transition: background-color 0.3s;
+    cursor: pointer;
   }
-  </style>
+
+  .btn-round {
+    margin-right: 5px;
+  }
+
+  .btn:hover {
+    background-color: #ccc;
+  }
+
+  .btn i {
+    font-size: 18px;
+    color: #000;
+  }
+
+  /* Additional CSS for unique class names */
+
+  .item-image {
+    width: 100%;
+    height: 40%;
+  }
+
+  .sale-name {
+    font-size: 0.7rem;
+    color: rgb(103, 95, 95);
+  }
+
+  .brand {
+    font-weight: 500;
+  }
+
+  .price {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .ad {
+    margin: 50px 0;
+  }
+
+  .action {
+    margin-top: 10px;
+  }
+
+  .btn-group {
+    display: flex;
+  }
+</style>
