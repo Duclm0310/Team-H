0e65d604d3c70719e4e37243cfbb20ec3bbb76c2
nghiadepzaivietnam <NghiaNTGCH210333@fpt.edu.vn>
2023-06-21 11:50:11 +0700

    fe view designer

--- Gucci/resources/views/designer/create.blade.php
+++ Gucci/resources/views/designer/create.blade.php
@@ -1,23 +1,48 @@
 @extends('layout.layout')
-@section('tile','create new designer')
+@section('title', 'Create New Designer')
 @section('content')
 
-<form action="/designer" method="post">
-    @csrf
-    <div class="mb-3">
-        <label for="name" class="form-label">Design Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Design Name">
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Designer Email</label>
-        <input type="text" class="form-control" id="email" name="email" placeholder="Designer Email">
-    </div>
-    <div class="mb-3">
-        <label for="style" class="form-label">Designer Style</label>
-        <input type="text" class="form-control" id="style" name="style" placeholder="Designer Style">
-    </div>
+<style>
+    .background-image {
+        background-image: url('{{ asset('img/bg.jpg') }}');
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+        margin: 0;
+    }
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
+    .card {
+        background-color: rgba(255, 255, 255, 0.8);
+        max-width: 400px;
+        padding: 20px;
+        border-radius: 10px;
+    }
+</style>
+<div class="background-image">
+    <div class="card">
+        <h2 class="text-center">Create New Designer</h2>
+        <form action="/designer" method="post">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Design Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Design Name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Designer Email</label>
+                <input type="text" class="form-control" id="email" name="email" placeholder="Designer Email">
+            </div>
+            <div class="mb-3">
+                <label for="style" class="form-label">Designer Style</label>
+                <input type="text" class="form-control" id="style" name="style" placeholder="Designer Style">
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
--- Gucci/resources/views/designer/index.blade.php
+++ Gucci/resources/views/designer/index.blade.php
@@ -1,37 +1,60 @@
 @extends('layout.layout')
-@section('tile','Designer')
+@section('title', 'Designer')
 @section('content')
 
-<div class="table-responsive">
-    <table class="table table-striped table-hover">
-        <thead class="table-light">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Action</th>
-            </tr>
+<div class="container">
+    <h1 class="text-center">Designer</h1>
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead class="table-light">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
             </thead>
-            <tbody class="table-group-divider">
-                  @foreach ($designers as $designer)
-                  <tr class="table-primary" >
+            <tbody>
+                @foreach ($designers as $designer)
+                <tr>
                     <td>
-                      <a href="{{url("/designer/".$designer->id)}}">
-                      {{$designer->name}}
-                      </a>
+                        <a href="{{url("/designer/".$designer->id)}}">
+                            {{$designer->name}}
+                        </a>
                     </td>
                     <td>{{$designer->email}}</td>
                     <td>
-                      <a href="{{url("/designer/".$designer->id)}}" class="btn btn-primary">View</a>
-                      <a href="{{url("/designer/".$designer->id."/edit")}}" class="btn btn-warning">Edit</a>
-                      <form action="{{url("/designer/".$designer->id)}}" method="post" class="d-inline">
-                        {{ method_field('DELETE') }}
-                        @csrf
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">Delete</button>
-                      </form>
+                        <a href="{{url("/designer/".$designer->id)}}" class="btn btn-primary btn-sm"><i class="fa fa-eye"></i></a>
+                        <a href="{{url("/designer/".$designer->id."/edit")}}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
+                        <form action="{{url("/designer/".$designer->id)}}" method="post" class="d-inline">
+                            {{ method_field('DELETE') }}
+                            @csrf
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?');"><i class="fa fa-trash"></i></button>
+                        </form>
                     </td>
-                  </tr>
-                  @endforeach
+                </tr>
+                @endforeach
             </tbody>
-    </table>
-  </div>
+        </table>
+    </div>
+</div>
+
+<style>
+    .container {
+        margin-top: 20px;
+    }
+
+    .table th {
+        background-color: #f8f9fa;
+        border-color: #dee2e6;
+    }
+
+    .table td {
+        vertical-align: middle;
+    }
+
+    .btn-sm {
+        padding: 0.25rem 0.5rem;
+        font-size: 0.875rem;
+    }
+</style>
 @endsection
