29cc4efb7a52a9d6da764c5c04a04ce876ca4866
nghiadepzaivietnam <NghiaNTGCH210333@fpt.edu.vn>
2023-06-21 11:47:02 +0700

    fe view material

--- Gucci/resources/views/material/create.blade.php
+++ Gucci/resources/views/material/create.blade.php
@@ -1,18 +1,65 @@
 @extends('layout.layout')
-@section('tile','create new product')
+@section('title', 'Create New Product')
 @section('content')
 
-<form method="post" action="/material">
-    @csrf
-    <div class="mb-3">
-        <label for="title" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Material name">
-    </div>
-    <div class="mb-3">
-      <label for="biography" class="form-label">Uses</label>
-      <textarea class="form-control" id="uses" name="uses" rows="3"></textarea>
-  </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
 
-  @endsection
+    .background-image {
+        background-image: url('{{ asset('img/bg.jpg') }}');
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+        margin: 0;
+    }
+
+    .card {
+        background-color: rgba(255, 255, 255, 0.8);
+        max-width: 400px;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    .form-label {
+        font-weight: bold;
+    }
+
+    .btn-primary {
+        background-color: #007bff;
+        border-color: #007bff;
+    }
+
+    .btn-primary:hover {
+        background-color: #0069d9;
+        border-color: #0062cc;
+    }
+</style>
+
+<div class="background-image">
+    <div class="card">
+        <h2 class="text-center">Create New Product</h2>
+        <form method="post" action="/material">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Material name">
+            </div>
+            <div class="mb-3">
+                <label for="uses" class="form-label">Uses</label>
+                <textarea class="form-control" id="uses" name="uses" rows="3"></textarea>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
--- Gucci/resources/views/material/index.blade.php
+++ Gucci/resources/views/material/index.blade.php
@@ -1,47 +1,57 @@
 @extends('layout.layout')
-@section('tile','Material')
+@section('title', 'Material')
 @section('content')
-<div class="table-responsive">
-   <table class="table table-striped
-   table-hover	
-   table-borderless
-   table-primary
-   align-middle">
-       <thead class="table-light">
-           <h1 class="h1">Material</h1>
-           <tr>
+<div class="container">
+   <h1 class="text-center">Material</h1>
+   <div class="table-responsive">
+      <table class="table table-striped table-hover">
+         <thead class="thead-light">
+            <tr>
                <th>Id</th>
                <th>Name</th>
                <th>Uses</th>
                <th>Action</th>
-           </tr>
-           </thead>
-           <tbody class="table-group-divider">
-             @foreach ($material as $material)
-               <tr class="table-primary" >
-                 <td>{{$material->id}}</td>
-                 <td>
-                   <a>
-                     {{$material->name}}
-                   </a>
-                 </td>
-                   <td>{{$material->uses}}</td>
-                   <td>
-                     <button><a href="{{url("/material/".$material->id)}}">Show</a></button>
-                     <button><a href="{{url("/material/".$material->id."/edit")}}">Edit</a></button>
-                     <form method="post" action="{{url("/material/".$material->id)}}" class="d-inline">
+            </tr>
+         </thead>
+         <tbody>
+            @foreach ($material as $material)
+            <tr>
+               <td>{{$material->id}}</td>
+               <td>{{$material->name}}</td>
+               <td>{{$material->uses}}</td>
+               <td>
+                  <a href="{{url("/material/".$material->id)}}" class="btn btn-primary btn-sm"><i class="fa fa-eye"></i></a>
+                  <a href="{{url("/material/".$material->id."/edit")}}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
+                  <form method="post" action="{{url("/material/".$material->id)}}" class="d-inline">
                      {{method_field('DELETE')}}
                      @csrf
-                     <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Are u sure?')">Delete</button>
-                     </form>
-                   </td>
-               </tr>
-               @endforeach
-           </tbody>
-           <tfoot>
-               
-           </tfoot>
-   </table>
- </div>
+                     <button class="btn btn-danger btn-sm" type="submit" onclick="return confirm('Are you sure?')"><i class="fa fa-trash"></i></button>
+                  </form>
+               </td>
+            </tr>
+            @endforeach
+         </tbody>
+      </table>
+   </div>
+</div>
 
+<style>
+   .container {
+      margin-top: 20px;
+   }
+
+   .table th {
+      background-color: #f8f9fa;
+      border-color: #dee2e6;
+   }
+
+   .table td {
+      vertical-align: middle;
+   }
+
+   .btn-sm {
+      padding: 0.25rem 0.5rem;
+      font-size: 0.875rem;
+   }
+</style>
 @endsection
