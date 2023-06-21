456f29558fab9b0d10f237faff4c4a1dae94c1ed
nghiadepzaivietnam <NghiaNTGCH210333@fpt.edu.vn>
2023-06-21 11:44:03 +0700

    fe user create

--- Gucci/resources/views/user/create.blade.php
+++ Gucci/resources/views/user/create.blade.php
@@ -1,40 +1,79 @@
 @extends('layout.layout')
-@section('tile','create user')
+@section('title', 'Create New User')
 @section('content')
 
-<form method="post" action="/user" role="form">
-    @csrf
-    <div class="mb-3">
-        <label for="title" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-    </div>
-    <div class="mb-3">
-      <label for="title" class="form-label">Email</label>
-      <input type="email" class="form-control" id="email" name="email" placeholder="Name">
-  </div>
-  <div class="form-outline mb-4">
-    <input type="password" name="password" id="password" class="form-control" />
-    <label class="form-label" for="pwd">Password</label>
-  </div>
+<style>
+    .background-image {
+        background-image: url('{{ asset('img/bg.jpg') }}');
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+        margin: 0;
+    }
 
-  <!-- Password re-input -->
-  <div class="form-outline mb-4">
-  <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" />
-  <label class="form-label" for="pwd">Repeate Password</label>
-  </div>
-  
-    <div class="mb-3">
-      <label for="biography" class="form-label">Role</label>
-      @foreach($role as $role)
-      <div class="checkbox">
-        <label>
-            <input type="checkbox" type="checkbox" name="role[]" value="{{$role->id}}">
-            {{$role->name}}
-        </label>
-      </div>
-      @endforeach
-  </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+    .card {
+        background-color: rgba(255, 255, 255, 0.8);
+        max-width: 400px;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    .form-label {
+        font-weight: bold;
+    }
+
+    .btn-primary {
+        background-color: #007bff;
+        border-color: #007bff;
+    }
+
+    .btn-primary:hover {
+        background-color: #0069d9;
+        border-color: #0062cc;
+    }
+</style>
+
+<div class="background-image">
+    <div class="card">
+        <h2 class="text-center">Create New User</h2>
+        <form method="post" action="/user" role="form">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+            </div>
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">Repeat Password</label>
+                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Repeat Password">
+            </div>
+            <div class="mb-3">
+                <label for="role" class="form-label">Role</label>
+                @foreach($role as $role)
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="role[]" value="{{$role->id}}">
+                        {{$role->name}}
+                    </label>
+                </div>
+                @endforeach
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 @endsection
